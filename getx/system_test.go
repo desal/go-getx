8a3fc7e1ab3061e7d234d2477e1270cb57221bf8
Daniel Salby <dsalby@gmail.com>
2016-05-03 21:44:33 +1000

    Moved core logic into 'getx' library

--- getx/system_test.go
+++ getx/system_test.go
@@ -1,4 +1,4 @@
-package main
+package getx
 
 import (
 	"testing"
--- go-getx.go
+++ go-getx.go
@@ -4,11 +4,10 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"strings"
 
 	"github.com/desal/cmd"
 	"github.com/desal/dsutil"
-	"github.com/desal/git"
+	"github.com/desal/go-getx/getx"
 	"github.com/desal/gocmd"
 	"github.com/desal/richtext"
 	"github.com/jawher/mow.cli"
@@ -18,177 +17,6 @@ import (
 //a/hats=http://server/special_repos/hats.git
 //a/([^/]+)=http://server/repos/$1.git
 //b/([^/]+)=http://other/repos/$1.git
-
-type empty struct{}
-type set map[string]empty
-type ScanMode int
-
-const (
-	ScanMode_Skip      ScanMode = iota //See a folder, assume it's good
-	ScanMode_DeepFetch                 //See a folder, check recursively for missing deps
-	ScanMode_Update                    //Update all deps we see if possible
-)
-
-type Context struct {
-	donePkgs set
-	install  bool
-	scanMode ScanMode
-	output   cmd.Output
-	goPath   []string
-	gitCtx   *git.Context
-	ruleSet  RuleSet
-}
-
-func NewContext(install bool, scanMode ScanMode, output cmd.Output, goPath []string, ruleSet RuleSet) *Context {
-	return &Context{
-		donePkgs: set{},
-		install:  install,
-		scanMode: scanMode,
-		output:   output,
-		goPath:   goPath,
-		gitCtx:   git.New(output),
-		ruleSet:  ruleSet,
-	}
-}
-
-func pkgContains(parent, child string) bool {
-	if parent == child || strings.HasPrefix(child, parent+"/") {
-		return true
-	}
-	return false
-}
-
-func (c *Context) AlreadyDone(pkg string) bool {
-	for donePkg, _ := range c.donePkgs {
-		if pkgContains(donePkg, pkg) {
-			return true
-		}
-	}
-	return false
-}
-
-func (c *Context) Get(workingDir, pkg string, depsOnly, tests bool) {
-	goCtx := gocmd.New(c.output, c.goPath) //TODO why am i creating this again?
-	goDir, alreadyExists := goCtx.Dir(workingDir, pkg)
-
-	if depsOnly && !alreadyExists {
-		c.output.Error("Can not get dependencies only for package %s, package does not exist", pkg)
-		os.Exit(1)
-	} else if depsOnly {
-		//Can do nothing
-		c.donePkgs[pkg] = empty{}
-	} else if !alreadyExists {
-		rootPkg, gitUrl, err := c.ruleSet.GetUrl(pkg)
-		if c.AlreadyDone(rootPkg) {
-			c.donePkgs[pkg] = empty{}
-			return
-		}
-		if err != nil {
-			c.output.Error(err.Error())
-			os.Exit(1)
-		}
-		if rootPkg != pkg {
-			c.Get(workingDir, rootPkg, depsOnly, tests)
-			return
-		}
-
-		err = c.gitCtx.Clone(goDir, gitUrl, false)
-		if err != nil {
-			c.donePkgs[pkg] = empty{}
-			c.donePkgs[rootPkg] = empty{}
-			return
-		}
-		c.donePkgs[pkg] = empty{}
-		c.donePkgs[rootPkg] = empty{}
-
-	} else if c.scanMode == ScanMode_Update || c.scanMode == ScanMode_DeepFetch {
-		gitTopLevel, err := c.gitCtx.TopLevel(goDir, false)
-		if err != nil {
-			c.donePkgs[pkg] = empty{}
-			return
-		}
-
-		list, err := goCtx.List(gitTopLevel, "")
-		if err != nil {
-			c.donePkgs[pkg] = empty{}
-			return
-		}
-
-		if len(list) != 1 {
-			panic("go list with no args should return a single package")
-		}
-
-		var rootPkg string
-		for e, _ := range list { //jump into the only element
-			rootPkg = e
-		}
-
-		if c.AlreadyDone(rootPkg) {
-			c.donePkgs[pkg] = empty{}
-			return
-		} else if rootPkg != pkg {
-			c.Get(workingDir, rootPkg, depsOnly, tests)
-			c.donePkgs[pkg] = empty{}
-			return
-		}
-
-		if c.scanMode == ScanMode_Update {
-			gitStatus, err := c.gitCtx.Status(goDir, false)
-			if err == nil && gitStatus == git.Clean {
-				c.gitCtx.Pull(goDir, false)
-			}
-		}
-
-		c.donePkgs[rootPkg] = empty{}
-		c.donePkgs[pkg] = empty{}
-
-	} else if c.scanMode == ScanMode_Skip {
-		c.donePkgs[pkg] = empty{}
-		return
-	}
-
-	list, _ := goCtx.List(workingDir, pkg+"/...")
-	for _, e := range list {
-		//Only check imports, because the recursive nature of this tool
-		//will get the transisitive dependencies.
-		var imports []interface{}
-		var testImports []interface{}
-		if importsInt, ok := e["Imports"]; ok {
-			imports = importsInt.([]interface{})
-		}
-
-		if testImportsInt, ok := e["TestImports"]; ok {
-			testImports = testImportsInt.([]interface{})
-		}
-
-		for _, impInt := range imports {
-			imp := impInt.(string)
-			if !goCtx.IsStdLib(imp) && !c.AlreadyDone(imp) {
-				c.Get(workingDir, imp, false, false)
-			}
-		}
-		if tests {
-			for _, impInt := range testImports {
-				imp := impInt.(string)
-				if !goCtx.IsStdLib(imp) && !c.AlreadyDone(imp) {
-					c.Get(workingDir, imp, false, false)
-				}
-			}
-		}
-	}
-	if c.install {
-		//attempt to install everything; takes advantage of multiple cores
-		//but will bomb out if some of the sub pkgs are particularly broken
-		//if that happens, attempt installing one by one instead
-		err := goCtx.Install(workingDir, pkg+"/...")
-		if err != nil {
-			for importPath, _ := range list {
-				goCtx.Install(workingDir, importPath)
-			}
-		}
-
-	}
-}
 
 func main() {
 	app := cli.App("gogetx", "go get extended")
@@ -212,7 +40,7 @@ func main() {
 			os.Exit(0)
 		}
 
-		ruleSet, err := LoadRulesFromFile(filepath.Join(dsutil.UserHomeDir(), ".go-getx-map"))
+		ruleSet, err := getx.LoadRulesFromFile(filepath.Join(dsutil.UserHomeDir(), ".go-getx-map"))
 		if err != nil {
 			fmt.Println(err.Error())
 			os.Exit(1)
@@ -220,17 +48,17 @@ func main() {
 
 		output := cmd.NewStdOutput(*verbose, richtext.Ansi())
 
-		var scanMode ScanMode
+		var scanMode getx.ScanMode
 		if *fetch {
-			scanMode = ScanMode_DeepFetch
+			scanMode = getx.ScanMode_DeepFetch
 		} else if *update {
-			scanMode = ScanMode_Update
+			scanMode = getx.ScanMode_Update
 		} else {
-			scanMode = ScanMode_Skip
+			scanMode = getx.ScanMode_Skip
 		}
 
 		goPath := gocmd.FromEnv(output)
-		ctx := NewContext(*install, scanMode, output, goPath, ruleSet)
+		ctx := getx.NewContext(*install, scanMode, output, goPath, ruleSet)
 		for _, pkg := range *pkgs {
 			ctx.Get(".", pkg, *dependencies, *tests)
 		}
